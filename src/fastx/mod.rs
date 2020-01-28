5d34c10c11898729e82a6374b3476b79d0185886
James Hawley <jamesrichardhawley@gmail.com>
2020-01-27 23:52:51 -0500

    Linting on reading from path
    * Trying to read from zipped FASTQ, no success

--- src/fastx/mod.rs
+++ src/fastx/mod.rs
@@ -1,14 +1,14 @@
 use bio::io::fastq;
 use crate::utils::detect_filetype;
 
-fn fx_header_type(_header: &str) -> &str
-{
-    return "";
-}
+// fn fx_header_type(_header: &str) -> &str
+// {
+//     return "";
+// }
 
-pub fn fx_info(_file: &str)
+pub fn fx_info(_path: &str)
 {
-    let reader = fastq::Reader::from_file(_file).unwrap();
+    let reader = fastq::Reader::from_file(_path).unwrap();
     let mut total: usize = 0;
     for _record in reader.records() {
         let record = _record.unwrap_or_else(|_| fastq::Record::new());
@@ -16,6 +16,6 @@ pub fn fx_info(_file: &str)
             total += 1;
         }
     }
-    println!("{}", detect_filetype(_file));
+    println!("{}", detect_filetype(_path));
     println!("{} reads", total);
 }
