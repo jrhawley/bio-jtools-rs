7108a68c0d6666336c60f05776a0a6a9ee4bba8c
James Hawley <jamesrichardhawley@gmail.com>
2020-01-27 22:35:23 -0500

    Fixing bug where empty lines were counted in FASTX files
    Needed to use `unwrap_or_else` and `is_empty` methods

--- src/fastx/mod.rs
+++ src/fastx/mod.rs
@@ -1,11 +1,20 @@
 use bio::io::fastq;
 use crate::utils::detect_filetype;
 
-pub fn fx_info(_file: &str) {
+fn fx_header_type(_header: &str) -> &str
+{
+    return "";
+}
+
+pub fn fx_info(_file: &str)
+{
     let reader = fastq::Reader::from_file(_file).unwrap();
     let mut total: usize = 0;
     for _record in reader.records() {
-        total += 1;
+        let record = _record.unwrap_or_else(|_| fastq::Record::new());
+        if !record.is_empty() {
+            total += 1;
+        }
     }
     println!("{}", detect_filetype(_file));
     println!("{} reads", total);
