af10f781cbe0952f261e7868dc31c3b40c0e2184
James Hawley <jamesrichardhawley@gmail.com>
2020-05-26 17:31:17 -0400

    Swapping from `fastq` to `needletail` crate
    * Simpler API, handles zipped files natively
    * Also counting number of bases in the FASTQ

--- src/fastx/mod.rs
+++ src/fastx/mod.rs
@@ -1,33 +1,43 @@
-use fastq::parse_path;
+// use fastq::parse_path;
+use needletail::parse_sequence_path;
 use std::collections::HashSet;
+use std::path::Path;
+use std::str;
 use std::string::String;
 
-pub fn info<'a>(_path: &str)
-{
+pub fn info<'a>(path: &Path) {
     // values to keep and display
-    let mut total: usize = 0;                               // number of records
-    let mut instruments: HashSet<String> = HashSet::new();  // sequencing instrument IDs
+    let mut n_records: usize = 0; // number of records
+    let mut n_bases: usize = 0; // number of bases
+    let mut instruments: HashSet<String> = HashSet::new(); // sequencing instrument IDs
 
     // parse the FASTQ
-    parse_path(Some(_path), |parser| {
-        // function over each record in the FASTQ
-        parser.each(|record| {
-            // add to total count
-            total += 1;
-            // convert RefRecord into OwnedRecord to access the Record's fields
-            let _orecord = record.to_owned_record();
-            // keep the record's ID for string manipulation to extract the machine ID
-            let _id = String::from_utf8(_orecord.head).unwrap();
+    parse_sequence_path(
+        path,
+        |_| {},
+        |seq| {
+            // add to n_records count
+            n_records += 1;
+            // keep track of the n_records number of bases
+            n_bases += seq.seq.len();
             // parse the instruments machine ID (always at the beginning of a read ID for the new Illumina encodings)
-            let _id_vals = _id.split(|c| c == ':' || c == ' ' || c == '.').take(1).collect::<Vec<&str>>()[0].to_string();
+            let id_val = str::from_utf8(
+                seq.id
+                    .split(|c| c == &b':' || c == &b' ' || c == &b'.')
+                    .next()
+                    .unwrap(),
+            )
+            .unwrap();
             // if a new intrument ID is detected
-            if !instruments.contains(&_id_vals) {
-                instruments.insert(_id_vals.to_string());
+            if !instruments.contains(id_val) {
+                instruments.insert(id_val.to_string());
             }
-            true
-        });
+        },
+    )
+    .expect("Error parsing FASTQ. Possible invalid compression.");
 
-        println!("{} reads", total);
-        println!("Instruments: {:?}", instruments);
-    }).expect("Error parsing FASTQ. Possible invalid compression.");
+    // print output
+    println!("{} bases", n_bases);
+    println!("{} reads", n_records);
+    println!("Instruments: {:?}", instruments);
 }
