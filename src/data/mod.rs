be44fdfa34ad91715aa7ba7ee2862fe9b2ed3b31
James Hawley <jamesrichardhawley@gmail.com>
2020-05-30 18:22:25 -0400

    Creation of reserved files

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-// use std::fs::rename;
+use std::fs::OpenOptions;
 use std::path::Path;
 
 type Date = chrono::NaiveDate;
@@ -45,9 +45,9 @@ pub fn organize(indir: &Path, seqtype: &str) {
     let reserved_filenames = vec!["README.md", "cluster.yaml", "Snakefile", "setup.log", "config.tsv"];
     let fq_regex = Regex::new(r"^([A-Za-z0-9-_]+)_S([1-9][0-9]?)_L00(\d)_(I[1-3]|R[1-3])_001\.f(ast)?q(\.gz)?$").unwrap();
     let dir_regex = Regex::new(r"^([0-9]{2})(0?[1-9]|1[012])(0[1-9]|[12]\d|3[01])_(\w{3,})_(\d{4})_(A|B)(\w{9})(.*)?").unwrap();
-    let dir_stem = indir.file_stem().unwrap().to_str().unwrap();
+    let dir_stem = indir.file_stem().unwrap();
     // extract flowcell information from directory name
-    let cap = dir_regex.captures(dir_stem).unwrap();
+    let cap = dir_regex.captures(dir_stem.to_str().unwrap()).unwrap();
     let date = Date::parse_from_str(
         &format!("{}{}{}", cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str(), cap.get(3).unwrap().as_str()),
         "%y%m%d"
@@ -60,6 +60,15 @@ pub fn organize(indir: &Path, seqtype: &str) {
         flowcell: cap.get(7).unwrap().as_str(),
         description: cap.get(8).unwrap().as_str(),
     };
-    println!("{:?}", sd);
-    println!("{}", date);
+    // create non-existant reserved files
+    for f in reserved_filenames {
+        let p = indir.join(Path::new(&f));
+        if !p.exists() {
+            OpenOptions::new().write(true).create_new(true).open(p).expect("Error creating file.");
+        }
+    }
+    // create non-existant reserved directories
+    // find and relocate FASTQs, if necessary
+    // extract sample information from FASTQs
+
 }
