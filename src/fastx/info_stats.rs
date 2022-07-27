71942b1d538e189f71b5b297b140ca8773796c97
James Hawley <developer@jrhawley.ca>
2022-07-27 09:14:26 -0400

    Big refactor on processing FastqStats
    - Breaking individual steps out into their own functions
    - Will make for easier testing and debugging later
    - Initial tests for this module

--- src/fastx/info_stats.rs
+++ src/fastx/info_stats.rs
@@ -1,7 +1,7 @@
 //! Statistics to record when parsing info about an HTS file
 
 use needletail::{errors::ParseError, parser::SequenceRecord};
-use std::{collections::HashMap, io::Read};
+use std::{collections::HashMap, io::Read, slice::Split};
 
 use super::InfoOpts;
 
@@ -61,89 +61,137 @@ impl FastqStats {
         rec: &Result<SequenceRecord, ParseError>,
         opts: &InfoOpts,
     ) {
-        match rec {
-            Ok(seq) => {
-                self.valid_records += 1;
+        if let Ok(seq) = rec {
+            self.process_valid_record(seq, opts);
+        } else {
+            self.process_invalid_record();
+        }
+    }
 
-                let seq_length: u64 = seq.num_bases().try_into().unwrap();
-                self.bases += seq_length;
+    /// Process the statistics for a valid record
+    fn process_valid_record(&mut self, seq: &SequenceRecord, opts: &InfoOpts) {
+        self.valid_records += 1;
 
-                if opts.lengths {
-                    match self.lengths.get_mut(&seq_length) {
-                        Some(v) => {
-                            *v += 1;
-                        }
-                        None => {
-                            self.lengths.insert(seq_length, 1);
-                        }
+        let seq_length: u64 = seq.num_bases().try_into().unwrap();
+        self.bases += seq_length;
+
+        if opts.lengths {
+            self.update_lengths(seq_length);
+        }
+        if opts.flow_cell_ids || opts.instruments {
+            // split the byte string by " "
+            let rname_separator_ascii_code = 32;
+
+            let mut splits = seq.id().split(|x| *x == rname_separator_ascii_code);
+
+            match (splits.next(), splits.next(), splits.next()) {
+                (Some(_), Some(_), Some(_)) => self.process_sra_split_record(),
+                (Some(a), Some(_), None) => self.process_illumina_split_record(a, opts),
+                (Some(_), None, None) => self.process_illumina_pre_v1_8_split_record(),
+                _ => todo!(),
+            };
+        }
+    }
+
+    /// Update the information about the lengths of records
+    fn update_lengths(&mut self, seq_length: u64) {
+        if let Some(v) = self.lengths.get_mut(&seq_length) {
+            *v += 1;
+        } else {
+            self.lengths.insert(seq_length, 1);
+        }
+    }
+
+    /// Process the statistics for an invalid record
+    fn process_invalid_record(&mut self) {
+        self.invalid_records += 1;
+    }
+
+    /// Process a Sequence Read Archive FASTQ record
+    fn process_sra_split_record(&mut self) {
+        // Sequence Read Archive ID will be ignored since there is no
+        // way to figure out what the original flow cell IDs were
+    }
+
+    /// Process an Illumina (Casava >= v1.8) formatted FASTQ record
+    fn process_illumina_split_record(&mut self, rname: &[u8], opts: &InfoOpts) {
+        // split the first element of the byte string by ":"
+        let illumina_separator_ascii_code = 58;
+
+        // Illumina Casava >= v1.8 format
+        let mut id_splits = rname.split(|x| *x == illumina_separator_ascii_code);
+
+        // instrument name
+        if opts.instruments {
+            self.process_illumina_instrument(&mut id_splits);
+        }
+
+        // run ID
+        id_splits.next();
+
+        // flow cell ID
+        if opts.flow_cell_ids {
+            self.process_illumina_flowcell(&mut id_splits);
+        }
+    }
+
+    fn process_illumina_flowcell<P>(&mut self, id_splits: &mut Split<'_, u8, P>)
+    where
+        P: FnMut(&u8) -> bool,
+    {
+        if let Some(mut s) = id_splits.next() {
+            let mut fcid = String::new();
+
+            // wait until the last possible moment to store the flow cell ID as a string
+            if s.read_to_string(&mut fcid).is_ok() {
+                // track that this flow cell is used
+                match self.flow_cell_ids.get_mut(&fcid) {
+                    Some(v) => {
+                        *v += 1;
+                    }
+                    None => {
+                        self.flow_cell_ids.insert(fcid, 1);
                     }
                 }
-
-                if opts.flow_cell_ids || opts.instruments {
-                    // split the byte string by " "
-                    let mut splits = seq.id().split(|x| *x == 32);
-
-                    match (splits.next(), splits.next(), splits.next()) {
-                        (Some(a), Some(b), Some(c)) => {
-                            // Sequence Read Archive ID will be ignored since there is no
-                            // way to figure out what the original flow cell IDs were
-                        }
-                        (Some(a), Some(b), None) => {
-                            // Illumina Casava >= v1.8 format
-                            // split the first element of the byte string by ":"
-                            let mut id_splits = a.split(|x| *x == 58);
-
-                            // instrument name
-                            if opts.instruments {
-                                if let Some(mut s) = id_splits.next() {
-                                    let mut instrument_name = String::new();
-
-                                    // wait until the last possible moment to store the instrument name as a string
-                                    if s.read_to_string(&mut instrument_name).is_ok() {
-                                        // track that the instrument is being used
-                                        match self.instruments.get_mut(&instrument_name) {
-                                            Some(v) => {
-                                                *v += 1;
-                                            }
-                                            None => {
-                                                self.instruments.insert(instrument_name, 1);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-
-                            // run ID
-                            id_splits.next();
-
-                            // flow cell ID
-                            if opts.flow_cell_ids {
-                                if let Some(mut s) = id_splits.next() {
-                                    let mut fcid = String::new();
-
-                                    // wait until the last possible moment to store the flow cell ID as a string
-                                    if s.read_to_string(&mut fcid).is_ok() {
-                                        // track that this flow cell is used
-                                        match self.flow_cell_ids.get_mut(&fcid) {
-                                            Some(v) => {
-                                                *v += 1;
-                                            }
-                                            None => {
-                                                self.flow_cell_ids.insert(fcid, 1);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        (Some(a), None, None) => todo!(),
-                        _ => todo!(),
-                    };
-                }
-            }
-            Err(_) => {
-                self.invalid_records += 1;
             }
         }
+    }
+
+    fn process_illumina_instrument<P>(&mut self, id_splits: &mut Split<'_, u8, P>)
+    where
+        P: FnMut(&u8) -> bool,
+    {
+        if let Some(mut s) = id_splits.next() {
+            let mut instrument_name = String::new();
+
+            // wait until the last possible moment to store the instrument name as a string
+            if s.read_to_string(&mut instrument_name).is_ok() {
+                // track that the instrument is being used
+                match self.instruments.get_mut(&instrument_name) {
+                    Some(v) => {
+                        *v += 1;
+                    }
+                    None => {
+                        self.instruments.insert(instrument_name, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    /// Process an Illumina (Casava < v1.8) formatted FASTQ record
+    fn process_illumina_pre_v1_8_split_record(&mut self) {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let expected = 4;
+        let observed = 2 + 2;
+
+        assert_eq!(expected, observed);
     }
 }
