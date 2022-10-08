a19e0cdbb2411b3673c03ac8dd1de6285870f70d
James Hawley <developer@jrhawley.ca>
2022-10-08 15:03:05 -0400

    Temporarily moving `filter` function into a new module
    This will eventually be deprecated and removed after the new filter implementation is complete.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -146,7 +146,9 @@ impl HtsFile {
                 let mut writer = SamWriter::from_path(out, reader.header().clone()).unwrap();
                 align::filter::filter(&mut reader, ids, &mut writer, keep)
             }
-            (Hts::Fastx(_), Some(Hts::Fastx(_))) => fastq::filter::filter(self, ids, out, keep),
+            (Hts::Fastx(_), Some(Hts::Fastx(_))) => {
+                fastq::filter::old_filter::filter(self, ids, out, keep)
+            }
             _ => unimplemented!(),
         }
     }
