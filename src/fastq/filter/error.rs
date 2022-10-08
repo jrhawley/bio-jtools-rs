cf54d555539759015bfc2a2897227425261b23fa
James Hawley <developer@jrhawley.ca>
2022-10-08 14:59:31 -0400

    Refactoring FASTQ filtering into its own module
    Moving errors into its own module, too.
    Using this opportunity to expand the types of errors being encountered.

---
+++ src/fastq/filter/error.rs
@@ -0,0 +1,30 @@
+//! Error handling when filtering records from a FASTQ file.
+
+use thiserror::Error;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum FastqFilterError {
+    #[error("Cannot specify both a regular expression and a file with exact IDs.")]
+    CannotSpecifyRegexAndIdFile,
+
+    #[error("You must filter against something, like a regular expression or file containing exact IDs.")]
+    FilterCannotBeEmpty,
+
+    #[error("ID file is required but not provided.")]
+    IdFileNotProvidedWhenRequired,
+
+    #[error("ID file cannot be opened.")]
+    IdFileCannotBeOpened,
+
+    #[error("Cannot parse first line in ID file.")]
+    CannotParseFirstIdFileLine,
+
+    #[error("ID file is empty.")]
+    EmptyIdFile,
+
+    #[error("Cannot parse first record in FASTQ file.")]
+    CannotParseFirstFastqRecord,
+
+    #[error("FASTQ file is empty.")]
+    EmptyFastqFile,
+}
