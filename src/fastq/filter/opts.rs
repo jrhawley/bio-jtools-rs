e81e5182c0d022e32a151d970e2a9ea31d60535d
James Hawley <developer@jrhawley.ca>
2022-10-13 16:50:31 -0400

    Second refactored attempt to filter the FASTQ

--- src/fastq/filter/opts.rs
+++ src/fastq/filter/opts.rs
@@ -3,7 +3,11 @@
 use crate::{cli::CliOpt, fastq::filter::FastqFilterError, record::filter::RecordFilter};
 use anyhow::bail;
 use clap::Parser;
-use needletail::{parser::FastqReader, FastxReader};
+use needletail::{
+    errors::ParseError,
+    parser::{FastqReader, SequenceRecord},
+    FastxReader,
+};
 use regex::Regex;
 use std::{
     fs::File,
@@ -65,53 +69,70 @@ impl RecordFilter for FastqFilterOpts {
 impl FastqFilterOpts {
     /// Filter out records using a sorted ID file to match against.
     fn filter_with_id_file(&self) -> anyhow::Result<()> {
+        let n_records_processed = 0u32;
         let mut id_reader = self.get_id_file_lines()?;
         let mut fq_reader = self.get_hts_reader();
-
         let mut filt_iter = FastqFilterIter::new();
-
-        let mut deal_with_remaining_reads = false;
-
         // writer for the output file (or STDOUT)
         let mut writer = self.writer_output()?;
+        let mut deal_with_remaining_reads = false;
 
-        // initialize the iter struct
-        filt_iter.get_next_id(&mut id_reader)?;
-        filt_iter.get_next_record(&mut fq_reader)?;
+        // The current record being processed.
+        // This belongs in this scope instead of inside `filt_iter` because of
+        // mutable references to data owned by `fq_reader`.
+        let mut curr_record = fq_reader.next();
 
         // this loop iterates over both files simultaneously
-        while filt_iter.curr_record().is_some() && filt_iter.curr_filter_id().is_some() {
-            filt_iter.assert_ids_are_sorted()?;
-            filt_iter.assert_records_are_sorted()?;
-            if filt_iter.curr_record_id() < filt_iter.curr_filter_id() {
-                // if the record is before the filtering ID and we are not keeping the filtering IDs
-                // then we should write it to the output
-                if !self.keep {
-                    // unwrap is guaranteed because of the while loop condition
-                    filt_iter.curr_record().unwrap().write(&mut writer, None)?;
-                }
+        loop {
+            let mut update_record = false;
+            let mut update_id = false;
 
-                // update the records
-                filt_iter.get_next_record(&mut fq_reader);
-            } else if filt_iter.curr_record_id() > filt_iter.curr_filter_id() {
-                // if the record is after the filtering ID, don't do anything with the current record right away
-                filt_iter.get_next_id(&mut id_reader)?;
+            assert_records_are_sorted(&curr_record, filt_iter.prev_record_id())?;
 
-                // if there are no more IDs to process, be sure to deal with the remaining records
-                // outside of the present loop
-                if filt_iter.curr_filter_id().is_none() {
-                    // unwrap is guaranteed because of the while loop condition
-                    filt_iter.curr_record().unwrap().write(&mut writer, None)?;
-                    deal_with_remaining_reads = true;
-                }
-            } else {
-                if self.keep {
-                    // unwrap is guaranteed because of the while loop condition
-                    filt_iter.curr_record().unwrap().write(&mut writer, None)?;
-                }
+            if let (Some(Ok(rec)), Some(filter_id)) = (curr_record, filt_iter.curr_filter_id()) {
+                if rec.id() < filter_id {
+                    // if the record is before the filtering ID and we are not keeping the filtering IDs
+                    // then we should write it to the output
+                    if !self.keep {
+                        rec.write(&mut writer, None)?;
+                    }
 
-                // update the records
-                filt_iter.get_next_record(&mut fq_reader)?;
+                    // update the records
+                    update_record = true;
+                } else if rec.id() > filter_id {
+                    // update the IDs
+                    update_id = true;
+                } else {
+                    if self.keep {
+                        // unwrap is guaranteed because of the while loop condition
+                        rec.write(&mut writer, None)?;
+                    }
+
+                    // update the records
+                    update_record = true;
+                }
+            }
+
+            // if update_record {
+            //     filt_iter.set_prev_record_id(&curr_record)?;
+            //     curr_record = fq_reader.next();
+            // } else if update_id {
+            //     // if the record is after the filtering ID, don't do anything with the current record right away
+            //     filt_iter.get_next_id(&mut id_reader)?;
+
+            //     // if there are no more IDs to process, be sure to deal with the remaining records
+            //     // outside of the present loop
+            //     if filt_iter.curr_filter_id().is_none() {
+            //         // unwrap is guaranteed because of the while loop condition
+            //         curr_record.unwrap().unwrap().write(&mut writer, None)?;
+            //         deal_with_remaining_reads = true;
+            //     }
+            // }
+
+            // if the updated current record or current filtering ID is `None`, then our comparisons are done
+            // and we can break out of the look
+            if curr_record.is_none() || filt_iter.curr_filter_id().is_none() {
+                break;
             }
         }
 
@@ -160,5 +181,23 @@ impl FastqFilterOpts {
     /// Filter out records based on its quality scores.
     fn filter_qual(&self) -> anyhow::Result<()> {
         Ok(())
+    }
+}
+
+/// Check that the FASTQ records are in sorted order.
+fn assert_records_are_sorted(
+    curr_record: &Option<Result<SequenceRecord, ParseError>>,
+    prev_record_id: Option<&[u8]>,
+) -> Result<(), FastqFilterError> {
+    match (curr_record, prev_record_id) {
+        (Some(Ok(curr)), Some(prev)) => {
+            if curr.id() < prev {
+                Ok(())
+            } else {
+                Err(FastqFilterError::FastqNotSorted)
+            }
+        }
+        (Some(Err(e)), _) => Err(FastqFilterError::CannotParseFastqRecord(e.clone())),
+        (_, _) => Ok(()),
     }
 }
