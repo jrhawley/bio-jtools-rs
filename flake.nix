e6dc172eabeddafe875f2e79b128084043d660db
James Hawley <developer@jrhawley.ca>
2023-02-20 01:25:06 -0500

    Adding direnv .envrc using nix flakes

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "bio-jtools configuration";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (import rust-overlay)
+          (self: super: {
+            rustToolchain = let rust = super.rust-bin;
+            in if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default;
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-audit
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            cargo-nextest
+            rust-analyzer
+          ];
+
+          shellHook = "";
+        };
+      }
+  );
+}
+
